d96f6dd43a1bf4f6115e5101dc81b84a80b7615f
Djordje Mihajlovic <djdjdjordje@gmail.com>
2024-04-02 14:13:15 +0100

    updates

--- H3S/src/H3S.py
+++ H3S/src/H3S.py
@@ -29,3 +29,37 @@ plt.gca().xaxis.set_ticks_position('both')
 plt.gca().yaxis.set_ticks_position('both')
 
 plt.show()
+
+data_points = np.linspace(3, 33, 11)
+print(data_points)
+for k in data_points:
+    f = open(f"../data/h3s_dos_tetra_{int(k)}.dat", "r")
+    h3s_e = []
+    h3s_dos_1 = []
+    h3s_dos_2 = []
+
+    for lines in f:
+        if lines[0] == "#":
+            continue
+        x = str.split(lines)
+        h3s_e.append(float(x[0]))
+        h3s_dos_1.append(float(x[1]))
+
+    f.close()
+
+    sns.set_style("whitegrid")
+    plt.plot(h3s_dos_1, h3s_e, "-", color="black")
+    plt.xlabel(r'DOS d(E)')
+    plt.ylabel(r"Energy (ev)")
+    plt.title(f"Energy DOS for H3S at k point {k}")
+    plt.gca().tick_params(which="both", direction="in", right=True, top=True)
+    plt.gca().xaxis.set_minor_locator(AutoMinorLocator())
+    plt.gca().yaxis.set_minor_locator(AutoMinorLocator())
+    plt.gca().xaxis.set_ticks_position('both')
+    plt.gca().yaxis.set_ticks_position('both')
+    plt.savefig(f"../plots/h3s.e-dos-{int(k)}.png")
+    plt.clf()
+
+
+
+
